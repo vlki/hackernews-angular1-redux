2114d437d604a792dc7eb3c33907eac332f9d86c
Jan Vlcek <vlki@vlki.cz>
2016-05-02 00:40:13 +0200

    Removed unused modules

--- app/todos/index.js
+++
@@ -1,8 +0,0 @@
-import angular from 'angular'
-
-const todosModule = angular.module('app.todos', [])
-
-import duckProvide from './todos.duck.js'
-duckProvide(todosModule)
-
-export default todosModule.name
